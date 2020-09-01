5d900600f87bf060f96ef849f24f7b9b5bd26257
Matt Smith <shaggydoog3@gmail.com>
2020-09-01 16:44:50 -0700

    Added initial docker and django files.

---
+++ scripts/scratch.sh
@@ -0,0 +1,5 @@
+# This is not intended to be run, but is a reference for some of the commands
+# I may need to run as I'm developing this app.
+
+# create the project:
+docker-compose run web django-admin startproject gamedb .
